e672e49dc2f848c0a055257a2ebee1818f9a8423
zeroday0619 <zeroday0619_dev@outlook.com>
2022-08-11 15:10:45 +0900

    Add script

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ wget 'https://discordapp.com/api/download?platform=linux&format=deb' -O discord.
 sudo alien --scripts -r -v -g discord.deb
 sudo sed -i 's#%dir "/"##' discord/discord-2.spec
 sudo sed -i 's#%dir "/usr/bin/"##' discord/discord-2.spec
-sudo rpmbuild --target=x86_64 --buildroot ~/Downloads/discord/ -bb discord-0.0.9-2.spec
+sudo rpmbuild --target=x86_64 --buildroot discord/ -bb discord-0.0.9-2.spec
