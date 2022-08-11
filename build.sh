0086272eb98acb64af403458c43333465a33113c
Euiseo Cha <zeroday0619_dev@outlook.com>
2022-08-11 15:15:10 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -x
 
 wget 'https://discordapp.com/api/download?platform=linux&format=deb' -O discord.deb
 sudo alien --scripts -r -v -g discord.deb
-sudo sed -i 's#%dir "/"##' discord/discord-2.spec
-sudo sed -i 's#%dir "/usr/bin/"##' discord/discord-2.spec
-sudo rpmbuild --target=x86_64 --buildroot discord/ -bb discord-0.0.9-2.spec
+sudo sed -i 's#%dir "/"##' discord/discord-*-2.spec
+sudo sed -i 's#%dir "/usr/bin/"##' discord/discord-*-2.spec
+sudo rpmbuild --target=x86_64 --buildroot discord-*/ -bb discord-*-2.spec
