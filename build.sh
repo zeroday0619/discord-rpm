734fa9bbe45cd8e2d158c778849463fe64d480a2
Euiseo Cha <zeroday0619_dev@outlook.com>
2023-02-16 09:15:32 +0900

    feat: update script

--- build.sh
+++ build.sh
@@ -9,3 +9,4 @@ sudo sed -i 's#%dir "/"##' discord-*/discord-*-2.spec
 sudo sed -i 's#%dir "/usr/bin/"##' discord-*/discord-*-2.spec
 cd discord-*
 sudo rpmbuild --target=x86_64 --buildroot -bb discord-*-2.spec
+sudo cp -r discord-*/usr/ /
