cba70985c45254ad6a40a015ed763216d2cad2ad
Euiseo Cha <zeroday0619_dev@outlook.com>
2024-03-17 21:13:25 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ if ! [[ -r $(pwd)/discord.deb ]]; then
     wget 'https://discordapp.com/api/download?platform=linux&format=deb' -O discord.deb
 fi
 
-sudo alien --scripts -r -v -g discord.deb
-sudo sed -i 's#%dir "/"##' $(pwd)/discord-*/discord-*-2.spec
-sudo sed -i 's#%dir "/usr/bin/"##' $(pwd)/discord-*/discord-*-2.spec
-cd $(pwd)/discord-*
-sudo rpmbuild --target=x86_64 --buildroot -bb discord-*-2.spec
+alien --scripts -r -v -g discord.deb
+sed -i 's#%dir "/"##' $(pwd)/discord-*/discord-*-2.spec
+sed -i 's#%dir "/usr/bin/"##' $(pwd)/discord-*/discord-*-2.spec
+echo "%changelog" | tee -a $(pwd)/discord-*/discord-*-2.spec
+echo "* "$(LC_ALL=c date +"%a")" "$(LC_ALL=c date +"%b")"   "$(LC_ALL=c date +"%d")" "$(LC_ALL=c date +"%Y")" ""Euiseo Cha <zeroday0619_dev@outlook.com> - repackaging version 0.0.1" | tee -a $(pwd)/discord-*/discord-*-2.spec
+echo "- Auto packaging" | tee -a $(pwd)/discord-*/discord-*-2.spec
+rpmbuild --buildroot $(pwd)/discord-* -bb $(pwd)/discord-*/discord-*-2.spec
